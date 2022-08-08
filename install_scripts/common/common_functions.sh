971e911f66d54585c409edb78f8e256cda1f4a5e
Marcio <oscensores@gmail.com>
2022-08-08 19:34:31 -0300

    Docker fixes

--- install_scripts/common/common_functions.sh
+++ install_scripts/common/common_functions.sh
@@ -1,4 +1,4 @@
-# This file contains common functions shared by many of the 
+# This file contains common functions shared by many of the
 # install scripts
 #
 # This script assumes certain BASH variables will be defined before
@@ -16,86 +16,86 @@ function get_basic_info() {
 	username=$SUDO_USER
 
 	# number of cores to use for make
-	cores=`nproc`
+	cores=$(nproc)
 }
 
 function exit_unless_root() {
 	if [[ $EUID != 0 ]]; then
-        	echo "You are attempting to run the script as user."
-        	echo "Please run with sudo:"
-        	echo "  sudo -E ./$script_name"
-        	exit 1
+		echo "You are attempting to run the script as user."
+		echo "Please run with sudo:"
+		echo "  sudo -E ./$script_name"
+		exit 1
 	fi
 }
 
 function exit_if_root() {
 	if [[ $EUID == 0 ]]; then
-        	echo "You are attempting to run the script as root."
-        	echo "Please do not run with sudo, but simply run:"
-        	echo "  ./$script_name"
-        	exit 1
+		echo "You are attempting to run the script as root."
+		echo "Please do not run with sudo, but simply run:"
+		echo "  ./$script_name"
+		exit 1
 	fi
 }
 
 function check_env_vars() {
-	# there should also be an environment variable for the target and 
+	# there should also be an environment variable for the target and
 	# source paths; if there is not, quit
 	if [[ -z "$SDR_TARGET_DIR" ]]; then
-        	echo "ERROR: Environment variable \$SDR_TARGET_DIR not defined."
-        	echo "       You should run ./grc_from_source.sh before running"
-        	echo "       this script. If you've already done that, you may"
-	       	echo "       need to open a new terminal and try this script"
-	       	echo "       again."
-        	exit 1
+		echo "ERROR: Environment variable \$SDR_TARGET_DIR not defined."
+		echo "       You should run ./grc_from_source.sh before running"
+		echo "       this script. If you've already done that, you may"
+		echo "       need to open a new terminal and try this script"
+		echo "       again."
+		exit 1
 	fi
 
 	if [[ -z "$SDR_SRC_DIR" ]]; then
-        	echo "ERROR: Environment variable \$SDR_SRC_DIR not defined."
-        	echo "       You should run ./grc_from_source.sh before running"
-        	echo "       this script. If you've already done that, you may"
-	       	echo "       need to open a new terminal and try this script"
-	       	echo "       again."
-        	exit 1
+		echo "ERROR: Environment variable \$SDR_SRC_DIR not defined."
+		echo "       You should run ./grc_from_source.sh before running"
+		echo "       this script. If you've already done that, you may"
+		echo "       need to open a new terminal and try this script"
+		echo "       again."
+		exit 1
 	fi
 
 	if [[ -z "$GRC_38" ]]; then
-        	echo "ERROR: Environment variable \$GRC_38 not defined."
-        	echo "       You should run ./grc_from_source.sh before running"
-        	echo "       this script. If you've already done that, you may"
-	       	echo "       need to open a new terminal and try this script"
-	       	echo "       again."
-        	exit 1
+		echo "ERROR: Environment variable \$GRC_38 not defined."
+		echo "       You should run ./grc_from_source.sh before running"
+		echo "       this script. If you've already done that, you may"
+		echo "       need to open a new terminal and try this script"
+		echo "       again."
+		exit 1
 	fi
 }
 
 # this function clones and builds code from a cmake-style git repo
 function clone_and_build() {
-        git_repo_url=$1
-        release_commit=$2 # can be either a release or commit
+	git_repo_url=$1
+	release_commit=$2 # can be either a release or commit
 	user_mode=$3      # calling script is sudo or user
-        cmake_args=$4     # in addition to the prefix build
+	cmake_args=$4     # in addition to the prefix build
 	prefix=$5         # defaults to $SDR_TARGET_DIR
 
 	# change directories to current source dir
 	cd $SDR_SRC_DIR
 
-        # clone the repo and cd into new the directory 
+	# clone the repo and cd into new the directory
 	if [[ $user_mode == "user" ]]; then
 		git clone --recursive $git_repo_url
 	else
-        	sudo -u "$username" git clone --recursive $git_repo_url
+		sudo -u "$username" git clone --recursive $git_repo_url
 	fi
 
-        cd `basename $git_repo_url`
+	cd $(basename $git_repo_url)
 
 	# if there is a specific release specified, check it out
 	if [[ ! -z $2 ]]; then
-		if [[  $user_mode == "user" ]]; then
-        		git checkout $release_commit
-        		git submodule update
+		if [[ $user_mode == "user" ]]; then
+			git checkout $release_commit
+			git submodule update
 		else
-        		sudo -u "$username" git checkout $release_commit
-        		sudo -u "$username" git submodule update
+			sudo -u "$username" git checkout $release_commit
+			sudo -u "$username" git submodule update
 		fi
 	fi
 
@@ -107,29 +107,27 @@ function clone_and_build() {
 		CMAKE_TARGET=$5
 	fi
 
-
-        # build it
-	if [[  $user_mode == "user" ]]; then
-        	mkdir build
-        	cd build
-        	cmake -DCMAKE_INSTALL_PREFIX=$CMAKE_TARGET \
-                                  	$cmake_args ../
-        	make -j$CORES
-        	make install
+	# build it
+	if [[ $user_mode == "user" ]]; then
+		mkdir build
+		cd build
+		cmake -DCMAKE_INSTALL_PREFIX=$CMAKE_TARGET \
+			$cmake_args ../
+		make -j$CORES
+		make install
 	else
-        	sudo -u "$username" mkdir build
-        	cd build
-        	sudo -u "$username" cmake -DCMAKE_INSTALL_PREFIX=$CMAKE_TARGET \
-                                  	$cmake_args ../
-        	sudo -u "$username" make -j$CORES
-        	sudo -u "$username" make install
+		sudo -u "$username" mkdir build
+		cd build
+		sudo -u "$username" cmake -DCMAKE_INSTALL_PREFIX=$CMAKE_TARGET \
+			$cmake_args ../
+		sudo -u "$username" make -j$CORES
+		sudo -u "$username" make install
 	fi
 }
 
-
 # return the ubuntu version number
 function get_ubuntu_version() {
-	ubuntu_version_str=`hostnamectl | grep -i Ubuntu`
+	ubuntu_version_str=$(hostnamectl | grep -i Ubuntu)
 	regex_str='Ubuntu\s+([A-Za-z0-9]+)'
 	if [[ ! "$ubuntu_version_str" =~ $regex_str ]]; then
 		echo "ERROR: Not running Ubuntu. Install scripts will require"
